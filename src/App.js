51bd9f20b6196bab69f6b08cd480f8cdeb38041c
Aneta <a.szmidt95@gmail.com>
2021-04-07 14:51:17 +0200

    schema

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+
+    state = {
+        contacts: [],
+    }
+
+    componentDidMount() {
+        this.getJsonFromApiAdvanced()
+    }
+
+    getJsonFromApiAdvanced() {
+    fetch("api/ser")
+        .then(res => res.json())
+        .then(json =>
+            {
+
+                this.setState({
+                    contacts: this.state.contacts.concat(json)
+                })
+                console.log(json)
+
+            }
+        );
+
+}
+    wordToWordItem  = (word) => {
+        const title = word.name
+
+        return <WordItem key={title} title={title} showItemsDescription={this.showItemsDescription}/>;
+    }
+
+    render() {
+        return (
+            <div className="gamePage">
+                hej
+
+                <div className="wordList" >
+                    {this.state.contacts.map(this.wordToWordItem)}
+                </div>
+
+            </div>
+        );
+    }
+
+
 }
 
-export default App;
+export const WordItem = ({title, showItemsDescription}) => {
+    return (
+        <div className="wordItem" onClick={showItemsDescription}>
+            <h2 className="header">{title}</h2>
+        </div>
+    );
+};
+
